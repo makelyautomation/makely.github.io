c6e0de8e785faa6a1fa38da7c2a0034ffd1a45e1
Makely <makely.automation@gmail.com>
2025-10-22 20:59:59 -0500

    fix: remove embedded repo folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Necesario para que GitHub Pages resuelva correctamente las rutas
-  base: '/makely-landing-core-energy/',
+  base: '/', // para repos del tipo makelyautomation.github.io
 })
